0a60e5b74706e118dfe53cd02e49cce7ea47d17e
Luis A. Flores <luis.a.floress@hotmail.com>
2019-03-07 15:57:01 -0600

    Tablas

---
+++ src/tablas.py
@@ -0,0 +1,60 @@
+# linear algebra
+import numpy as np 
+
+# data processing
+import pandas as pd
+
+# data visualization
+import seaborn as sns
+
+from matplotlib import pyplot as plt
+from matplotlib import style
+
+train_df = pd.read_csv("train.csv",sep=',')
+
+survived = 'survived'
+not_survived = 'not survived'
+fig, axes = plt.subplots(nrows=1, ncols=2,figsize=(10, 4))
+women = train_df[train_df['Sex']=='female']
+men = train_df[train_df['Sex']=='male']
+ax = sns.distplot(women[women['Survived']==1].Age.dropna(), bins=18, label = survived, ax = axes[0], kde =False)
+ax = sns.distplot(women[women['Survived']==0].Age.dropna(), bins=40, label = not_survived, ax = axes[0], kde =False)
+ax.legend()
+ax.set_title('Female')
+ax = sns.distplot(men[men['Survived']==1].Age.dropna(), bins=18, label = survived, ax = axes[1], kde = False)
+ax = sns.distplot(men[men['Survived']==0].Age.dropna(), bins=40, label = not_survived, ax = axes[1], kde = False)
+ax.legend()
+_ = ax.set_title('Male')
+
+plt.show()
+
+
+
+FacetGrid = sns.FacetGrid(train_df, row='Embarked', size=4.5, aspect=1.6)
+FacetGrid.map(sns.pointplot, 'Pclass', 'Survived', 'Sex', palette=None,  order=None, hue_order=None )
+FacetGrid.add_legend()
+
+plt.show()
+
+sns.barplot(x='Pclass', y='Survived', data=train_df)
+
+plt.show()
+
+grid = sns.FacetGrid(train_df, col='Survived', row='Pclass', size=2.2, aspect=1.6)
+grid.map(plt.hist, 'Age', alpha=.5, bins=20)
+grid.add_legend();
+
+plt.show()
+
+data = [train_df]
+for dataset in data:
+    dataset['relatives'] = dataset['SibSp'] + dataset['Parch']
+    dataset.loc[dataset['relatives'] > 0, 'not_alone'] = 0
+    dataset.loc[dataset['relatives'] == 0, 'not_alone'] = 1
+    dataset['not_alone'] = dataset['not_alone'].astype(int)
+
+train_df['not_alone'].value_counts()
+
+axes = sns.factorplot('relatives','Survived', data = train_df, aspect = 2.5, )
+
+plt.show()
